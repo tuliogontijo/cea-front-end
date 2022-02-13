0b4b55afef64bbbfc996a78aa0d9892ccbfcd348
Cristina Kochmann <cristinak.iftm@gmail.com>
2022-02-13 16:19:06 -0300

    Criação da tela de login com sucesso by Cris

---
+++ src/pages/Login/index.js
@@ -0,0 +1,74 @@
+import { Form, Input } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+import logo from "../../images/logo.png";
+
+import Button from "../../components/Button";
+
+import "./styles.css";
+import { useNavigate } from 'react-router-dom';
+
+const { Item } = Form;
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+    navigate("/leads/listagem");
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+  return (
+    <div className="container">
+      <img src={logo} alt="logo" />
+      <h1>Painel Administrativo Comunidade Mães de Impacto</h1>
+      <Form
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        name="form-auth"
+      >
+        <Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor, insira seu login!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined />}
+            placeholder="Login" />
+        </Item>
+        <Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Por favor, insira sua senha!',
+            },
+          ]}
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            placeholder="Senha" />
+        </Item>
+        <Button
+          type="submit"
+          stylesButton="buttonPrimary buttonSubmit"
+        >
+          Entrar
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Login;
